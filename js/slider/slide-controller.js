ebe360fe7f37dd57e3680aea91c477a0f6c3e2c7
kaktus <ktsxinfo@gmail.com>
2020-01-22 23:41:59 +0200

    Update slide-controller.js

--- js/slider/slide-controller.js
+++ js/slider/slide-controller.js
@@ -18,6 +18,8 @@ function eventsHandler(){
     this.wheel = (e) => {
             e.preventDefault()
 
+            // Why you don't keep one coding style? Somewhere you put semicolon, somewhere no.
+            // Anyway you should call move, so calculat argument for it. Use simple conditional (ternary) operatorю
             if (e.deltaY > 0) scroller.move(1)
             else scroller.move(-1)
         }
@@ -30,11 +32,17 @@ function eventsHandler(){
     this.touchmove = (e) => {
         e.preventDefault()
 
+        // Semicolon is presented here
         const currentTouchY = e.targetTouches[0].clientY;
 
         if (!lastTouch.scrolled) {
+            // Semicolon isn't presented here
+            // What's the magic number (0.2)?  
             const swipingY = window.innerHeight * 0.2
 
+            // You should simplify it.
+            // Get calculated value once
+            // Put it to move method
             if (currentTouchY - swipingY > lastTouch.y) move(-1)
             else if ((currentTouchY + swipingY < lastTouch.y)) move(1)
 
